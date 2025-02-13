ff8c83d9c1c6e960c37e3a9d6cb91c1ea3294c41
Evgeny_Ch <gek_sh@mail.ru>
2025-02-14 00:26:30 +0300

    Added files from Spring_Initializr

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+	java
+	application
+	id("org.springframework.boot") version "3.3.8"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+application {
+	mainClass.set("hexlet.code.app.AppApplication")
+}
+
+group = "hexlet.code"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
