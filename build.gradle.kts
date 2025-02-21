27893159400edc3862938751f103910e3bd4f94e
Evgeny_Ch <gek_sh@mail.ru>
2025-02-21 22:07:06 +0300

    Added dependecies

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,15 @@ dependencies {
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	runtimeOnly("com.h2database:h2")
 	implementation("org.hibernate.validator:hibernate-validator:8.0.1.Final")
+	testImplementation(platform("org.junit:junit-bom:5.10.0"))
+	testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+	testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.2")
+	implementation("net.datafaker:datafaker:2.0.1")
+	implementation("org.instancio:instancio-junit:3.3.0")
+	implementation("org.mapstruct:mapstruct:1.5.5.Final")
+	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+	implementation("org.openapitools:jackson-databind-nullable:0.2.6")
+//	implementation("org.springframework.boot:spring-boot-starter-security")
 }
 
 tasks.withType<Test> {
