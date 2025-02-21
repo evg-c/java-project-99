305caa5379021347da0f056a82e690b334f198ba
Evgeny_Ch <gek_sh@mail.ru>
2025-02-21 22:09:30 +0300

    Audit annotations added

--- src/main/java/hexlet/code/app/model/User.java
+++ src/main/java/hexlet/code/app/model/User.java
@@ -2,10 +2,13 @@ package hexlet.code.app.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,6 +16,7 @@ import lombok.ToString;
 import jakarta.validation.constraints.Email;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
 
@@ -22,6 +26,7 @@ import java.time.LocalDate;
 @Setter
 @ToString(includeFieldNames = true, onlyExplicitlyIncluded = true)
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@EntityListeners(AuditingEntityListener.class)
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -33,13 +38,16 @@ public class User {
     private String firstName;
 
     @ToString.Include
-    private String lastNAme;
+    private String lastName;
 
+    @NotNull
     @ToString.Include
+    @EqualsAndHashCode.Include
     @Column(unique = true)
     @Email
     private String email;
 
+    @Size(min = 3)
     private String password;
 
     @CreatedDate
